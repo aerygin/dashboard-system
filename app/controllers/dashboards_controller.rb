522c891b3b7cd4f90946606419360fdfd83c52fe
artjomj <artjom.jerogin@icefire.ee>
2019-08-13 11:17:17 +0300

    create dashboard page added

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,26 @@
 class DashboardsController < ApplicationController
   def index
+    @dashboards = Dashboard.where(user_id: current_user.id)
+  end
 
+  def show
+
+  end
+
+  def new
+    @dashboard = current_user.dashboards.build
+  end
+
+  def create
+    @dashboard = current_user.dashboards.build(dashboard_params)
+    if @dashboard.save
+      redirect_to dashboards_path
+    end
+  end
+
+  private
+
+  def dashboard_params
+    params.require(:dashboard).permit(:title, :body)
   end
 end
